f257f7442832be74f48873f4872c30e02e5a26b6
Prathap Chilukuri <pcha01@L-IDC0C8FD59-M.local>
2017-06-11 11:59:30 +0530

    add timer and countdown

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,8 @@ var ReactDOM = require('react-dom');
 //below is similar to var Route = require('react-router').Route;
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 // Load foundation
 require('style!css!foundation-sites/dist/css/foundation.min.css')
@@ -14,6 +16,8 @@ require('style!css!sass!applicationStyles')
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <Route path="countdown" component={Countdown}/>
+      <IndexRoute component={Timer}/>
     </Route>
   </Router>,
   document.getElementById('app')
